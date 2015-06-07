31296415f159d5631bbff7335d3c9d0c3ed78c82
Mechtatel-student <Mechtatel_t@mail.ru>
2015-06-07 10:41:47 +0300

    Revert "Revert "ChangeMiner""
    This reverts commit fdeb9826608a4b24e83a2881289eddcc0cf3fc8b.

--- TaskEducation/WordsAndNumbers/Program.cs
+++ TaskEducation/WordsAndNumbers/Program.cs
@@ -21,9 +21,9 @@ namespace WordsAndNumbers
             Console.WriteLine("Enter text with words and numbers:");
             string str = Console.ReadLine();
             string str1 = str.Replace("  ", " ");
-            StringSplitOptions options = StringSplitOptions.None;
+            StringSplitOptions options = StringSplitOptions.RemoveEmptyEntries;
 
-            string[] st = str.Split(' ');
+            string[] st = str.Split(new char[] { ' ' }, options);
 
             Console.WriteLine(str);
             Console.WriteLine(str1);
@@ -112,5 +112,31 @@ namespace WordsAndNumbers
             Console.WriteLine(string.Join(" ", st2));
             Console.ReadKey();
         }
+        //public static void Main()
+        //{
+        //    Console.WriteLine("Press CTRL+C to exit, otherwise press any key.");
+        //    ConsoleKeyInfo cki;
+        //    do
+        //    {
+        //        cki = Console.ReadKey(true);
+        //        if (!Char.IsNumber(cki.KeyChar))
+        //        {
+        //            Console.WriteLine("Non-numeric input");
+        //        }
+        //        else
+        //        {
+        //            Int32 number;
+        //            if (Int32.TryParse(cki.KeyChar.ToString(), out number))
+        //            {
+        //                Console.WriteLine("Number received: {0}; <9? {1}", number, number < 9);
+        //            }
+        //            else
+        //            {
+        //                Console.WriteLine("Unable to parse input");
+        //            }
+        //        }
+        //    }
+        //    while (cki.KeyChar != 27);
+        //}
     }
 }
