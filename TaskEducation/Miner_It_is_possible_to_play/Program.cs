2b0593576243246eca1c6fcc8f87490480968498
Mechtatel-student <Mechtatel_t@mail.ru>
2015-06-11 11:54:59 +0300

    ClassField

--- TaskEducation/Miner_It_is_possible_to_play/Program.cs
+++ TaskEducation/Miner_It_is_possible_to_play/Program.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+
+
+
+
 namespace Miner_It_is_possible_to_play
 {
     class Program
@@ -31,10 +35,11 @@ namespace Miner_It_is_possible_to_play
         /// Игра "Минёр"
         /// </summary>
         /// <param name="isOpenCells">Параметр имеет значение истина, если игра ведётся до момента открытия всех клеток,
-        ///                и ложь, если игра ведётся до открытия всех пустых клеток без мин, .   </param>
+        ///                и ложь, если игра ведётся до открытия определённой по счету мины.   </param>
         /// <param name="mines"> Параметр указывает, до какой по счету открытой мины ведётся игра.  </param>
-        static void Game(bool isOpenCells = true, int mines = 0)
+        static void Game(bool isOpenCells = true, int mines = 1)
         {
+            
             Console.WriteLine("Enter count_of_rows and count_of_colunms:");
             Console.Write("Count_of_rows = ");
             int n = IsIntegerDiapason("Enter natural number:\r\n Count_of_rows = ", 1);
@@ -50,7 +55,8 @@ namespace Miner_It_is_possible_to_play
             countMines = IsIntegerDiapason("Enter natural number or 0:\r\n Pole can contain count of mines <= "
                                                 + (n * m) + "Count_of_mines = ", 0, (n * m));
 
-            int[,] a = Initialize(n, m, countMines);
+            
+            Field a = Initialize(n, m, countMines);
             PrintArray(a);
             Console.ReadKey();
             Console.Clear();
@@ -62,17 +68,14 @@ namespace Miner_It_is_possible_to_play
             Console.WriteLine();
             PrintColorPoleSelelction(a);
 
-            int countOpenCells = 0;
-            int countOpenMines = 0;
-
             do
             {
-                Console.WriteLine("Enter number_of_rou <= {0} and number_of_colunm <= {1}:", a.GetLength(0), a.GetLength(1));
+                Console.WriteLine("Enter number_of_rou <= {0} and number_of_colunm <= {1}:", a.GetWidth(), a.GetHeigth());
                 Console.Write("Number_of_row = ");
-                x = IsIntegerDiapason("Enter natural number <=" + a.GetLength(0) + " :\r\n Number_of_row = ", 0);
+                x = IsIntegerDiapason("Enter natural number <=" + a.GetWidth() + " :\r\n Number_of_row = ", 0);
 
                 Console.Write("Number_of_colunm = ");
-                y = IsIntegerDiapason("Enter natural number <=" + a.GetLength(1) + ":\r\n Number_of_row = ", 0);
+                y = IsIntegerDiapason("Enter natural number <=" + a.GetHeigth() + ":\r\n Number_of_row = ", 0);
 
                 if (x > 0 && y > 0)
                 {
@@ -80,21 +83,19 @@ namespace Miner_It_is_possible_to_play
 
                     Console.ForegroundColor = ConsoleColor.Green;
                     Console.WriteLine("You choose cell (" + x + "," + y + ")");
-                    if (a[x - 1, y - 1] >= 0)
+                    if (a.IsEmpty(x-1,y-1))
                     {
                         Console.ForegroundColor = ConsoleColor.Green;
                         Console.WriteLine("This cell is empty , safety, ");
                     }
                     else
                     {
-                        if (!isOpenCells)
-                            countOpenMines++;
                         Console.ForegroundColor = ConsoleColor.Magenta;
                         Console.WriteLine("This cell contain the mine !");
                     }
                     Console.ForegroundColor = prevFColor;
                     Console.WriteLine();
-                    countOpenCells += Change(a, x - 1, y - 1);
+                    a.OpenCell(x - 1, y - 1);
                     PrintColorPoleSelelction(a, x, y);
                     Console.WriteLine();
                 }
@@ -109,23 +110,23 @@ namespace Miner_It_is_possible_to_play
                     }
 
             }
-            while (Math.Pow(x, 2) + Math.Pow(y, 2) != 0 && countOpenCells < a.Length &&
-                                                     (isOpenCells || countOpenMines < mines));
-            if (Math.Pow(x, 2) + Math.Pow(y, 2) == 0)
+            while (Math.Abs(x) + Math.Abs(y) != 0 && a.CountOpenedCells() < a.GetCountCells() &&
+                                                     (isOpenCells || a.CountOpenedCellsWithMine() < mines));
+            if (Math.Abs(x) + Math.Abs(y)  == 0)
                 Console.WriteLine("You bring to end game, press \" Enter \" to quit. ");
             else
                 if (isOpenCells)
                     Console.WriteLine("You opened all cells, press \" Enter \" to quit. ");
 
                 else
-                    if (countOpenCells == a.Length)
+                    if (a.CountOpenedCells() == a.GetCountCells())
                     {
                         Console.WriteLine("You opened all cells, limit open mines more then count of mines, ");
                         Console.WriteLine(" press \" Enter \" to quit. ");
                     }
                     else
                         Console.WriteLine("You opened {0}  cells with mine, therefore game is end, " +
-                              " press \" Enter \" to quit. ", countOpenMines);
+                              " press \" Enter \" to quit. ", a.CountOpenedCellsWithMine());
 
             Console.ReadKey();
 
@@ -183,106 +184,41 @@ namespace Miner_It_is_possible_to_play
         /// <param name="b">Количество мин</param>
         /// <returns>Заполненный массив чисел</returns>
 
-        static int[,] Initialize(int n, int m, int b)
+        static Field Initialize(int n, int m, int b)
         {
 
-            int[,] res = new int[n, m];
-
-            for (int i = 0; i < res.GetLength(0); i++)
-                for (int j = 0; j < res.GetLength(1); j++)
-                    res[i, j] = 10;
+            Field res = new Field(n, m);
+                       
             Random r = new Random();
-            for (int i = 0; i < b; i++)
-            {
-                bool isEmpty = true;
-                int x = 0, y = 0;
-                do
+            int x = 0, y = 0;
+                do 
                 {
-                    isEmpty = true;
-                    x = r.Next(res.GetLength(0));
-                    y = r.Next(res.GetLength(1));
-
-                    if (res[x, y] > 0)
-                        res[x, y] = -20;
-                    else
-                        isEmpty = false;
+                    x = r.Next(res.GetWidth());
+                    y = r.Next(res.GetHeigth());
+                    res.SetMine(x, y);
                 }
-                while (!isEmpty);
-
-                for (int i1 = Math.Max(x - 1, 0); i1 < Math.Min(x + 2, res.GetLength(0)); i1++)
-                    for (int j1 = Math.Max(y - 1, 0); j1 < Math.Min(y + 2, res.GetLength(1)); j1++)
-                        res[i1, j1]++;
-
-            }
+                while (res.CountCellsWithMine()<b);
+            
             return res;
         }
-        /// <summary>
-        /// Выбор ячейки поля и изменение значений поля при выборе.
-        /// </summary>
-        /// <param name="a">Массив чисел, поле </param>
-        /// <param name="x">Номер строки </param>
-        /// <param name="y">Номер столбца </param>
-        /// <returns>Количество изменённых ячеек </returns>
-        static int Change(int[,] a, int x, int y)
-        {
-            int b = 1;
-            if (x >= 0 && x < a.GetLength(0) && y >= 0 && y < a.GetLength(1))
-            {
-                if (Math.Abs(a[x, y]) >= 10)
-                    a[x, y] = a[x, y] - 10 * Math.Sign(a[x, y]);
-                else
-                {
-                    b = 0;
-                    Console.WriteLine("You already change this cell");
-                }
-            }
-            else
-            {
-                b = 0;
-                Console.WriteLine("Error index");
-            }
-
-            return b;
-
-        }
+        
 
         /// <summary>
         ///  Вывод на экран массива чисел, соответствующего полю.
         /// </summary>
         /// <param name="a">Массив чисел</param>
-        static void PrintArray(int[,] a)
+        static void PrintArray(Field a)
         {
-            for (int i = 0; i < a.GetLength(0); i++)
+            for (int i = 0; i < a.GetWidth(); i++)
             {
                 Console.Write(" ");
-                for (int j = 0; j < a.GetLength(1); j++)
-                    Console.Write(a[i, j] + "\t");
+                for (int j = 0; j < a.GetHeigth(); j++)
+                    Console.Write(a.GetCells(i,j) + "\t");
                 Console.WriteLine();
 
             }
         }
-        /// <summary>
-        ///  Вывод на экран поля, с отображением выбранных и не выбранных ячеек.
-        /// </summary>
-        /// <param name="a">Массив чисел</param>
-        static void PrintPole(int[,] a)
-        {
-
-            for (int i = 0; i < a.GetLength(0); i++)
-            {
-                for (int j = 0; j < a.GetLength(1); j++)
-                    if (Math.Abs(a[i, j]) < 10)
-                        if (a[i, j] >= 0)
-                            Console.Write(a[i, j] + "\t");
-                        else
-                            Console.Write("+\t");
-                    else
-                        Console.Write(" \t");
-                Console.WriteLine();
-
-
-            }
-        }
+        
 
 
 
@@ -328,10 +264,10 @@ namespace Miner_It_is_possible_to_play
         /// <param name="x">Номер строки выбранной ячейки </param>
         /// <param name="y">Номер столбца выбранной ячейки
         /// </param>
-        static void PrintColorPoleSelelction(int[,] a, int x = 0, int y = 0)
+        static void PrintColorPoleSelelction(Field a, int x = 0, int y = 0)
         {
             Console.Write("     ");
-            for (int j = 0; j < a.GetLength(1); j++)
+            for (int j = 0; j < a.GetHeigth(); j++)
                 if (j < 9)
                     Console.Write((j + 1) + "  ");
                 else
@@ -345,10 +281,10 @@ namespace Miner_It_is_possible_to_play
             Console.ForegroundColor = ConsoleColor.Black;
 
             Console.Write("-");
-            for (int j = 0; j < a.GetLength(1); j++)
+            for (int j = 0; j < a.GetHeigth(); j++)
                 Console.Write("---");
             Console.WriteLine();
-            for (int i = 0; i < a.GetLength(0); i++)
+            for (int i = 0; i < a.GetWidth(); i++)
             {
 
                 Console.BackgroundColor = prevBColor;
@@ -361,17 +297,17 @@ namespace Miner_It_is_possible_to_play
                 Console.BackgroundColor = ConsoleColor.White;
                 Console.ForegroundColor = ConsoleColor.Black;
                 Console.Write("|");
-                for (int j = 0; j < a.GetLength(1); j++)
+                for (int j = 0; j < a.GetHeigth(); j++)
                 {
                     if (i == x - 1 && j == y - 1)
                         Console.BackgroundColor = ConsoleColor.Yellow;
                     else
                         Console.BackgroundColor = ConsoleColor.White;
-                    if (Math.Abs(a[i, j]) < 10)
-                        if (a[i, j] >= 0)
+                    if (a.IsOpened(i,j))
+                        if (a.IsEmpty(i,j))
                         {
-                            Console.ForegroundColor = CurrentColorSymvol(a[i, j]);
-                            Console.Write(a[i, j] + " ");
+                            Console.ForegroundColor = CurrentColorSymvol(a.CountMineAround(i,j));
+                            Console.Write(a.CountMineAround(i,j)+ " ");
                         }
                         else
                         {
@@ -395,7 +331,7 @@ namespace Miner_It_is_possible_to_play
                 Console.ForegroundColor = ConsoleColor.Black;
 
                 Console.Write("-");
-                for (int j = 0; j < a.GetLength(1); j++)
+                for (int j = 0; j < a.GetHeigth(); j++)
                     Console.Write("---");
                 Console.WriteLine();
             }
@@ -403,7 +339,6 @@ namespace Miner_It_is_possible_to_play
             Console.BackgroundColor = prevBColor;
             Console.ForegroundColor = prevFColor;
         }
-
 
     }
 }
